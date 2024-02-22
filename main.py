04df644c08b6c02161d978570294b0bb6cffde96
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-02-21 23:56:01 -0300

    try using flask

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from flask import Flask
 
-class App(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("Hello World", "utf-8"))
+app = Flask( __name__ )
 
-if __name__ == "__main__":
-    app = HTTPServer(("localhost", 8080), App)
-    app.serve_forever()
-    pass
+@app.route('/')
+def root():
+    return "<p>Hello World</p>"
+
+app.run()
 
